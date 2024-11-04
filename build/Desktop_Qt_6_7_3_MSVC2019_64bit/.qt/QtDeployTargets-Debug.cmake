ae8b5e460ff61f200f6efdd8ca3acf40e45a329c
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2024-11-04 12:12:56 +0330

    initial commit

---
+++ build/Desktop_Qt_6_7_3_MSVC2019_64bit/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_FILE C:/Users/Asus/Projects/Frequently Used Repos/AGV-Conflict-Free/build/Desktop_Qt_6_7_3_MSVC2019_64bit/Debug/AGV-Conflict-Free.exe)
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_RUNTIME_DLLS C:/Qt/6.7.3/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Cored.dll)
---
+++ build/Desktop_Qt_6_7_3_MSVC2019_64bit/.qt/QtDeployTargets-MinSizeRel.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_FILE C:/Users/Asus/Projects/Frequently Used Repos/AGV-Conflict-Free/build/Desktop_Qt_6_7_3_MSVC2019_64bit/MinSizeRel/AGV-Conflict-Free.exe)
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_AGV-Conflict-Free_RUNTIME_DLLS C:/Qt/6.7.3/msvc2019_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/msvc2019_64/bin/Qt6Core.dll)
